9901dc3ed1083f6f4a142d6c8537fb81eb801249
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-21 21:52:20 +0600

    Inital Commit

---
+++ app/src/main/java/com/sifat/loginandsignup/SignupActivity.kt
@@ -0,0 +1,52 @@
+package com.sifat.loginandsignup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.sifat.loginandsignup.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignupBinding
+    lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.directLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.signup.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this){ task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this, "Your Signup Successful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, LoginActivity2::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this, "Your Signup Unsuccessful", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this, "Enter Email/Password", Toast.LENGTH_SHORT).show()
+            }
+        }
+        setContentView(binding.root)
+    }
+}
