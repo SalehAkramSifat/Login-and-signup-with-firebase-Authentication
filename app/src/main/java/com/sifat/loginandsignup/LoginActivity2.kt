d4b5b6f424b51f61078f5221d7c474cab124043c
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-21 22:21:17 +0600

    Inital Commit

--- app/src/main/java/com/sifat/loginandsignup/LoginActivity2.kt
+++ app/src/main/java/com/sifat/loginandsignup/LoginActivity2.kt
@@ -1,20 +1,54 @@
 package com.sifat.loginandsignup
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.sifat.loginandsignup.databinding.ActivityLogin2Binding
 
 class LoginActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityLogin2Binding
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLogin2Binding.inflate(layoutInflater)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.sign.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+
+        binding.login.setOnClickListener {
+            val email = binding.email.text.toString().trim()
+            val password = binding.password.text.toString().trim()
+
+            if (email.isNotEmpty()&&password.isNotEmpty()){
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this){ task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this, "Login is Successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "Login is Unsuccessful", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this, "Enter email/password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
