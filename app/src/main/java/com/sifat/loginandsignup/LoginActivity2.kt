cd7a90667b19351db295d3b50d624c3713cf9a18
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-22 00:27:41 +0600

    Inital Commit

--- app/src/main/java/com/sifat/loginandsignup/LoginActivity2.kt
+++ app/src/main/java/com/sifat/loginandsignup/LoginActivity2.kt
@@ -5,20 +5,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.sifat.loginandsignup.databinding.ActivityLogin2Binding
+import android.util.Patterns
+import android.view.View
+import android.widget.ProgressBar
 
 class LoginActivity2 : AppCompatActivity() {
-    lateinit var binding: ActivityLogin2Binding
-    lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: ActivityLogin2Binding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        progressBar = binding.progressBar
 
         binding.sign.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
@@ -30,25 +34,36 @@ class LoginActivity2 : AppCompatActivity() {
             val email = binding.email.text.toString().trim()
             val password = binding.password.text.toString().trim()
 
-            if (email.isNotEmpty()&&password.isNotEmpty()){
-                firebaseAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this){ task ->
-                        if (task.isSuccessful){
-                            Toast.makeText(this, "Login is Successful", Toast.LENGTH_SHORT).show()
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
-                            finish()
-                        }
-                        else{
-                            Toast.makeText(this, "Login is Unsuccessful", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-            }
-            else{
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                if (isValidEmail(email)) {
+                    loginUser(email, password)
+                } else {
+                    Toast.makeText(this, "Enter Correct email", Toast.LENGTH_SHORT).show()
+                }
+            } else {
                 Toast.makeText(this, "Enter email/password", Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
-        setContentView(binding.root)
+    private fun loginUser(email: String, password: String) {
+        progressBar.visibility = View.VISIBLE
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                progressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Login is Successful", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Login is Unsuccessful", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
